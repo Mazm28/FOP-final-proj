4230af2f2f2f2d6cc2f0a3307d20a36d4d80725b
MoAli <meschi.ma24@gmail.com>
2024-01-20 19:10:03 +0330

    start of a jorney

---
+++ tig.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define config "config"
+#define init "init"
+#define add "add"
+#define rest "rest"
+#define reset "reset"
+#define status "status"
+#define commit "commit"
+#define set "set"
+#define replace "replace"
+#define remove "remove"
+#define log "log"
+#define branch "branch"
+#define checkout "checkout"
+
+int main(int argc, char **argv){
+    
+}
